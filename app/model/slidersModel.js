9946ae13bfaa57b0c586d1561207e2a334e9fc6d
Safayethossain95 <safayet.hossain95@gmail.com>
2024-11-12 14:13:12 +0600

    slidersmodel change

--- app/model/slidersModel.js
+++ app/model/slidersModel.js
@@ -6,7 +6,7 @@ const DataSchema = new mongoose.Schema(
         title:{type:String,required:true},
         des:{type:String,required:true},
         price:{type:String,required:true},
-        img:{type:String,required:true},
+        image:{type:String,required:true},
 
         productID:{type:mongoose.Schema.Types.ObjectId,required:true},
 
